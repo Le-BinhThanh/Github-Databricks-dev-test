c7f6cf08eaa4072d0939365db96e250ba1e61f06
let@ns.aucnet.co.jp <let@ns.aucnet.co.jp>
2023-05-09 08:28:04 +0000

    Test display query notebook

---
+++ Test display query Notebook/Test display query Notebook.sql
@@ -0,0 +1,17 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC # Test
+
+-- COMMAND ----------
+
+USE CATALOG hive_metastore
+
+-- COMMAND ----------
+
+SELECT *
+FROM auction_study_dev_lake.sample_transaction_data
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # End
